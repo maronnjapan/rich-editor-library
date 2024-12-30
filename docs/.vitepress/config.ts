86d4145e4494687941b11df33bc584afc6554408
maronnjapan <tihoutaikai20@gmail.com>
2024-12-31 01:24:08 +0900

    baseを削除

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/docs',
   title: "Rich Editor Library",
   description: "Lexicalをベースにしたリッチエディタライブラリです",
   themeConfig: {
